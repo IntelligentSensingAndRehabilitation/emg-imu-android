40ef9e5f83c570068e01b5a875cd417a1bb84e27
James Cotton <peabody124@gmail.com>
2018-07-18 19:08:42 -0500

    Revert "Android: switch to BLE library"
    This reverts commit 536433246737eda80eca0a007d52aaa15755b7bc.

--- emgimuservice/src/main/java/org/sralab/emgimu/service/EmgImuManager.java
+++ emgimuservice/src/main/java/org/sralab/emgimu/service/EmgImuManager.java
@@ -50,7 +50,6 @@ import java.util.UUID;
 
 import no.nordicsemi.android.log.Logger;
 import no.nordicsemi.android.ble.BleManager;
-import no.nordicsemi.android.ble.Request;
 
 import static java.lang.Math.abs;
 
