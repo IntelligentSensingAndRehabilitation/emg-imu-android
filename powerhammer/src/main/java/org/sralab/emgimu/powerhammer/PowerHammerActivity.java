ce2f10b0a799ff7d712837b4548d465bf76561d1
James Cotton <peabody124@gmail.com>
2018-12-30 11:54:23 -0600

    Unity: launcher extends UnityPlayerActivity
    Since we are using default behavior for most of this, we can
    simply extend it. The wrapper then creates the shim between
    the the EmgImuService and the Unity player.

---
+++ powerhammer/src/main/java/org/sralab/emgimu/powerhammer/PowerHammerActivity.java
@@ -0,0 +1,85 @@
+package org.sralab.emgimu.powerhammer;
+
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+import com.unity3d.player.*;
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.Window;
+
+import org.sralab.emgimu.service.EmgImuService;
+import org.sralab.emgimu.service.EmgImuServiceHolder;
+
+import io.fabric.sdk.android.Fabric;
+
+public class PowerHammerActivity extends UnityPlayerActivity
+{
+    private static final String TAG = PowerHammerActivity.class.getSimpleName();
+
+    private EmgImuServiceHolder mServiceHolder;
+
+    // Setup activity layout
+    @Override protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        CrashlyticsCore crashlyticsCore = new CrashlyticsCore.Builder()
+                .disabled(BuildConfig.DEBUG)
+                .build();
+        Fabric.with(this, new Crashlytics.Builder().core(crashlyticsCore).build());
+
+        // Create the service holder
+        Log.d(TAG, "Creating service holder");
+        mServiceHolder = new EmgImuServiceHolder(this);
+        mServiceHolder.onCreate();
+        mServiceHolder.setCallbacks(mEmgImuCallbacks);
+    }
+
+    // Pause Unity
+    @Override protected void onPause()
+    {
+        super.onPause();
+        mServiceHolder.onPause();
+    }
+
+    // Resume Unity
+    @Override protected void onResume()
+    {
+        super.onResume();
+        mServiceHolder.onResume();
+    }
+    private final EmgImuServiceHolder.Callbacks mEmgImuCallbacks = new EmgImuServiceHolder.Callbacks() {
+
+        @Override
+        public void onEmgPwrReceived(BluetoothDevice device, int value) {
+
+        }
+
+        @Override
+        public void onEmgClick(BluetoothDevice device) {
+            MotionEvent event = MotionEvent.obtain(SystemClock.uptimeMillis(),
+                    SystemClock.uptimeMillis(), MotionEvent.ACTION_BUTTON_PRESS,
+                    0, 0, 0);
+            //mUnityPlayer.
+                Log.d(TAG, "Received click, injecting an event: " + event);
+            mUnityPlayer.injectEvent(event);
+        }
+
+        @Override
+        public void onServiceBinded(EmgImuService.EmgImuBinder binder) {
+            Log.d(TAG, "Service bound");
+        }
+
+        @Override
+        public void onServiceUnbinded() {
+            Log.d(TAG, "Service unbound");
+        }
+    };
+}
