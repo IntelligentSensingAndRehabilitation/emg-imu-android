b78b626fb98100784eecdf1a743da2cef49ffde1
James Cotton <peabody124@gmail.com>
2019-06-08 19:36:01 -0500

    Android: NetworkStreaming threading issue fix

--- emgimuservice/src/main/java/org/sralab/emgimu/streaming/NetworkStreaming.java
+++ emgimuservice/src/main/java/org/sralab/emgimu/streaming/NetworkStreaming.java
@@ -1,6 +1,9 @@
 package org.sralab.emgimu.streaming;
 
 import android.bluetooth.BluetoothDevice;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.util.Log;
 
 import com.google.gson.Gson;
@@ -31,34 +34,77 @@ public class NetworkStreaming {
      * stored in the server logs.
      */
 
-    private ClientThread clientThread;
-    private Thread socketThread;
+    private HandlerThread handlerThread;
+    private Handler handler;
+
+    private Socket socket;
+    private OutputStream outputStream;
 
     public NetworkStreaming() {
         Log.d(TAG, "Streaming initialized.");
     }
 
     public void start(String ip_address, int port) {
-        clientThread = new ClientThread(ip_address, port);
-        socketThread = new Thread(clientThread);
-        socketThread.start();
+        handlerThread = new HandlerThread("NetworkStreaming Thread");
+        handlerThread.start();
+        Looper looper = handlerThread.getLooper();
+        handler = new Handler(looper);
+
+        handler.post(() -> {
+            InetAddress serverAddr = null;
+            try {
+                serverAddr = InetAddress.getByName(ip_address);
+            } catch (UnknownHostException e) {
+                Log.e(TAG, "Unknown host. Unable to connect to server.");
+                return;
+
+
+            }
+
+            try {
+                socket = new Socket(serverAddr, port);
+                outputStream = socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "Unable to open network socket");
+            }
+        });
     }
 
     public void stop() {
-        Log.d(TAG, "Stopping socket thread");
-        mRun = false;
-        socketThread.interrupt();
         try {
-            socketThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            socket.close();
+            socket = null;
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to close socket");
         }
+
+        handlerThread.interrupt();
+        handlerThread = null;
+        handler = null;
     }
 
     public boolean isConnected() {
-        if (clientThread == null)
+        if (socket == null)
             return false;
-        return clientThread.isConnected();
+        return socket.isConnected();
+    }
+
+    private void write(byte[] b) {
+        if (handler != null) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    //Log.d(TAG, "Writing " + b.length + " bytes and " + ByteBuffer.allocate(4).putInt(b.length).array().length);
+                    try {
+                        outputStream.write(ByteBuffer.allocate(4).putInt(b.length).array());
+                        outputStream.write(b);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
     }
 
     public void streamEmgBuffer(BluetoothDevice dev,
@@ -70,7 +116,7 @@ public class NetworkStreaming {
         Gson gson = new Gson();
         EmgRawMessage msg = new EmgRawMessage(dev.getAddress(), time, channels, samples, data);
         String json = gson.toJson(msg);
-        clientThread.write(json.getBytes());
+        write(json.getBytes());
     }
 
     public void streamEmgPwr(BluetoothDevice dev,
@@ -80,83 +126,14 @@ public class NetworkStreaming {
         Gson gson = new Gson();
         EmgPwrMessage msg = new EmgPwrMessage(dev.getAddress(), time, data);
         String json = gson.toJson(msg);
-        clientThread.write(json.getBytes());
+        write(json.getBytes());
     }
 
     public void streamTrackingXY(float x, float y) {
         Gson gson = new Gson();
         TrackingXYCoordinate xy = new TrackingXYCoordinate(x, y);
         String json = gson.toJson(xy);
-        clientThread.write(json.getBytes());
+        write(json.getBytes());
     }
 
-    private boolean mRun;
-
-    class ClientThread implements Runnable {
-
-        private Socket socket;
-        private int port;
-        private String ip_address;
-
-        private OutputStream outputStream;
-
-        public ClientThread(String ip_address, int port) {
-            this.ip_address = ip_address;
-            this.port = port;
-        }
-
-        @Override
-        public void run() {
-
-            mRun = true;
-
-            Log.d(TAG, "Opening socket. IP: " + this.ip_address + " Port: " + this.port);
-
-            try {
-                InetAddress serverAddr = InetAddress.getByName(this.ip_address);
-
-                socket = new Socket(serverAddr, this.port);
-                outputStream = socket.getOutputStream();
-
-                Log.d(TAG, "Streaming socket opened");
-
-                while(mRun) {
-                    synchronized (outputStream) {
-                        try {
-                            outputStream.wait(10);
-                        } catch (InterruptedException e) {
-                            Log.d(TAG, "Interruption indicates stopping requested.");
-                            e.printStackTrace();
-                        }
-                    }
-                }
-
-                socket.close();
-                Log.d(TAG, "Streaming socket closed");
-
-            } catch (UnknownHostException e1) {
-                e1.printStackTrace();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-
-        }
-
-        void write(byte[] b) {
-            try {
-                Log.d(TAG, "Writing " + b.length + " bytes and " + ByteBuffer.allocate(4).putInt(b.length).array().length);
-                outputStream.write(ByteBuffer.allocate(4).putInt(b.length).array());
-                outputStream.write(b);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        protected boolean isConnected() {
-            if (socket == null)
-                return false;
-            return socket.isConnected();
-        }
-
-    }
 }
