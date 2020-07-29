fa3181108446d14ea5c763dd2289ac26dc01d174
James Cotton <peabody124@gmail.com>
2020-07-29 17:05:03 -0500

    Android/UnityBindings: create stub

---
+++ unity_bindings/src/main/java/org/sralab/emgimu/unity_bindings/Bridge.java
@@ -0,0 +1,23 @@
+package org.sralab.emgimu.unity_bindings;
+
+//import needed packages / classes.
+import android.app.Application;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+public class Bridge extends Application
+{
+    // our native method, which will be called from Unity3D
+    public void PrintString( final Context ctx, final String message )
+    {
+        //create / show an android toast, with message and short duration.
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
