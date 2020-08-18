3a3df350d456e73bc7021dca617d46eab3092ba3
James Cotton <peabody124@gmail.com>
2020-08-18 16:03:37 -0500

    Android/Streaming: update to EmgImuViewModel

--- streaming/src/main/java/org/sralab/emgimu/streaming/DeviceViewModel.java
+++ streaming/src/main/java/org/sralab/emgimu/streaming/DeviceViewModel.java
@@ -15,6 +15,7 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.Transformations;
 
+import org.sralab.emgimu.EmgImuViewModel;
 import org.sralab.emgimu.service.EmgImuService;
 import org.sralab.emgimu.service.EmgPwrData;
 import org.sralab.emgimu.service.EmgStreamData;
@@ -29,117 +30,43 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
 
-public class DeviceViewModel extends AndroidViewModel {
+public class DeviceViewModel extends EmgImuViewModel<Device> {
 
     private final static String TAG = DeviceViewModel.class.getSimpleName();
 
-    private Application app;
-    Map<BluetoothDevice, Device> deviceMap;
-
-    public boolean isFiltering() {
-        return filtering;
-    }
-
-    public void setFiltering(boolean filtering) {
-        this.filtering = filtering;
-        for (Device d : deviceMap.values())
-            d.setFiltering(filtering);
-    }
-
-    private boolean filtering;
-
-    // Use mediator so we can connect observer at construction but
-    // provide real data source (via transformation) when connected
-    MediatorLiveData<List<Device>> devicesLiveData = new MediatorLiveData<>();
-    public LiveData<List<Device>> getDevicesLiveData() { return devicesLiveData; }
-
-    private IEmgImuServiceBinder service;
+    @Override
+    public boolean getObserveStream() { return true; }
 
     public DeviceViewModel(Application app) {
         super(app);
+    }
 
-        Log.d(TAG, "Created");
-
-        deviceMap = new HashMap<>();
-        devicesLiveData.setValue(new ArrayList<>(deviceMap.values()));
-
-        final Intent service = new Intent();
-        service.setComponent(new ComponentName("org.sralab.emgimu", "org.sralab.emgimu.service.EmgImuService"));
-        app.getApplicationContext().bindService(service, serviceConnection, Context.BIND_AUTO_CREATE);
-        this.app = app;
+    private boolean filtering = true;
+    public void setFiltering(boolean filtering) {
+        this.filtering = filtering;
+        for (Device d : getDeviceMap().values())
+            d.setFiltering(filtering);
     }
 
     @Override
-    protected void onCleared() {
-        Log.d(TAG, "onCleared");
-        super.onCleared();
-        try {
-            service.unregisterEmgStreamObserver(pwrObserver);
-        } catch (RemoteException e) {
-            throw new RuntimeException(e);
+    public void emgStreamUpdated(Device dev, EmgStreamData data) {
+        double [][] voltage = IntStream.range(0, data.channels)
+                .mapToObj(i -> Arrays.copyOfRange(data.voltage, i * data.samples, (i + 1) * data.samples))
+                .toArray(double[][]::new);
+
+        double ts = (double) data.ts;
+        for (int c = 0; c < data.channels; c++) {
+            for (int s = 0; s < data.samples; s++) {
+                dev.addVoltage(c, ts + s * 1000.0 / data.Fs, voltage[c][s]);
+            }
         }
-        this.app.getApplicationContext().unbindService(serviceConnection);
     }
 
-    private final IEmgImuStreamDataCallback.Stub pwrObserver = new IEmgImuStreamDataCallback.Stub() {
-        @Override
-        public void handleData(BluetoothDevice device, EmgStreamData data) {
-            Device dev = deviceMap.get(device);
-
-            double [][] voltage = IntStream.range(0, data.channels)
-                    .mapToObj(i -> Arrays.copyOfRange(data.voltage, i * data.samples, (i + 1) * data.samples))
-                    .toArray(double[][]::new);
-
-            double ts = (double) data.ts;
-            for (int c = 0; c < data.channels; c++) {
-                for (int s = 0; s < data.samples; s++) {
-                    dev.addVoltage(c, ts + s * 1000.0 / data.Fs, voltage[c][s]);
-                }
-            }
-        }
-    };
-
-    private ServiceConnection serviceConnection = new ServiceConnection() {
-
-        @Override
-        public void onServiceConnected(final ComponentName name, final IBinder binder) {
-            service = IEmgImuServiceBinder.Stub.asInterface(binder);
-
-            try {
-                service.registerEmgStreamObserver(pwrObserver);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-
-            // This accesses methods outside the AIDL scope
-            EmgImuService.EmgImuBinder fullBinding = (EmgImuService.EmgImuBinder) service;
-            LiveData<List<BluetoothDevice>> liveDevices = fullBinding.getLiveDevices();
-
-            deviceMap.clear();
-
-            LiveData<List<Device>> transformed = Transformations.map(liveDevices, input -> {
-
-                ArrayList<Device> output = new ArrayList<>();
-
-                for (final BluetoothDevice d : input) {
-                    Log.d(TAG, "Adding device: " + d.getAddress());
-
-                    Device dev = new Device(2);
-                    output.add(dev);
-                    dev.setFiltering(filtering);
-                    deviceMap.put(d, dev);
-                }
-
-                return output;
-            });
-
-            devicesLiveData.addSource(transformed, value -> devicesLiveData.setValue(value));
-        }
-
-        @Override
-        public void onServiceDisconnected(final ComponentName name) {
-            service = null;
-        }
-    };
+    @Override
+    public Device getDev(BluetoothDevice d) {
+        Device dev = new Device(2);
+        dev.setFiltering(filtering);
+        return dev;
+    }
 
 }
