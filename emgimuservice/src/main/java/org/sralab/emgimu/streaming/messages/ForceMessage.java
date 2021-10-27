5ee735058c27aa0be7daf1e2a86ff1d2cb5c6358
mechaBuilderSRAlab <vstepanov@ricres.org>
2021-10-27 12:25:44 -0500

    android: populated with code the ForceMessage,java class

--- emgimuservice/src/main/java/org/sralab/emgimu/streaming/messages/ForceMessage.java
+++ emgimuservice/src/main/java/org/sralab/emgimu/streaming/messages/ForceMessage.java
@@ -1,2 +1,15 @@
-package org.sralab.emgimu.streaming.messages;public class ForceMessage {
+package org.sralab.emgimu.streaming.messages;
+
+public class ForceMessage {
+
+    public final String MSG = "DynamometerForce";
+    public String bluetoothMac;
+    public long timestamp;
+    public double [] data;
+
+    public ForceMessage(String bluetoothMac, long timestamp, double [] data) {
+        this.bluetoothMac = bluetoothMac;
+        this.timestamp = timestamp;
+        this.data = data;
+    }
 }
