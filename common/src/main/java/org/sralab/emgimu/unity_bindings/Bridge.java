aacef7b1defebde44de75d51b4d89102cecc8ca1
Victor Stepanov <vstepanov@ricres.org>
2022-01-31 10:34:19 -0600

    [GUIS-10] Android: get Mac & chnl in Bridge.

--- common/src/main/java/org/sralab/emgimu/unity_bindings/Bridge.java
+++ common/src/main/java/org/sralab/emgimu/unity_bindings/Bridge.java
@@ -42,27 +42,27 @@ public class Bridge extends Application
 
             String device_mac = (String) device.toString();
             Log.d(TAG, "device_mac = " + device_mac + ", len(str) = " + device_mac.length());
-            Log.d(TAG, "unity_mac = " + unitySelectedDevice + ", len(str) = " + unitySelectedDevice.length());
-            if (device_mac.equals(unitySelectedDevice))
-            {
-                Log.d(TAG, "MATCH!!!! " + device_mac + "=" + unitySelectedDevice);
-                callback.onSuccess(Integer.toString(data.power[0]));
-            }
+            //Log.d(TAG, "unity_mac = " + unitySelectedDevice + ", len(str) = " + unitySelectedDevice.length());
 
-            if (callback != null) {
-                if (unitySelectedDevice != null) {
-                    Log.d(TAG, "Device was selected --> " + unitySelectedDevice);
-                    Log.d(TAG, "Current Device --> " + device);
-                    Log.d(TAG, "device.toString()=" + device.toString() + " | unitySelectedDevice=" + unitySelectedDevice);
-                    Log.d(TAG, "BOOOOOOOOOMM!");
 
-/*                    if (device.toString() == unitySelectedDevice)
-                    {
-                        Log.d(TAG, "We have a match! --> " + device.toString() + " = " + unitySelectedDevice);
-                    }*/
-                }
-                //callback.onSuccess(Integer.toString(data.power[0]));
-            }
+//            if (callback != null) {
+//                if (unitySelectedDevice != null) {
+//                    Log.d(TAG, "Device was selected --> " + unitySelectedDevice);
+//                    Log.d(TAG, "Current Device --> " + device);
+//                    Log.d(TAG, "device.toString()=" + device.toString() + " | unitySelectedDevice=" + unitySelectedDevice);
+//                    Log.d(TAG, "BOOOOOOOOOMM!");
+//                    if (device_mac.equals(unitySelectedDevice))
+//                    {
+//                        Log.d(TAG, "MATCH!!!! " + device_mac + "=" + unitySelectedDevice);
+//                        callback.onSuccess(Integer.toString(data.power[0]));
+//                    }
+///*                    if (device.toString() == unitySelectedDevice)
+//                    {
+//                        Log.d(TAG, "We have a match! --> " + device.toString() + " = " + unitySelectedDevice);
+//                    }*/
+//                }
+//                //callback.onSuccess(Integer.toString(data.power[0]));
+//            }
         }
     };
 
@@ -82,7 +82,7 @@ public class Bridge extends Application
                 callback.sendDeviceList(Arrays.toString(service.getManagedDevices().toArray()));
 
                 // stream data from all sensors
-                service.registerEmgPwrObserver(pwrObserver);
+//                service.registerEmgPwrObserver(pwrObserver);
                 //service.registerEmgStreamObserver(streamObserver);
             } catch (RemoteException e) {
                 throw new RuntimeException(e);
@@ -99,7 +99,9 @@ public class Bridge extends Application
     long startTime;
     String gameName;
     String gameLog;
-    public static String unitySelectedDevice;
+    private static String gameSelectedDeviceMac;
+    private static int gameSelectedDeviceChannel;
+
     public void logTrial(String roundInfo) {
         // expects to receive something that can be added to a list, which can
         // be serialized to JSON
@@ -135,9 +137,45 @@ public class Bridge extends Application
         callback = null;
     }
 
-    public void selectDeviceForUnityStreaming(String deviceMac) {
-        // sending device mac from unity for streaming
-        unitySelectedDevice = deviceMac;
-        Log.d(TAG, "unity selected device: " + unitySelectedDevice);
+    /**
+     * This method is called from the game, when the user selects the sensor and corresponding
+     * channel they wish to stream emg power value from. The main performs the following:
+     *  (1) parse the String parameter into a device MAC address and the corresponding channel.
+     *  (2) registers Emg power observer callback, which will start streaming the data for the
+     *      selected sensor.
+     * @param deviceMacAndChannel String representing a MAC address and the channel in the following
+     *                            format: XX:XX:XX:XX:XX:XX - ch-y, where each part is separated
+     *                            by a dash (" - "), respectively.
+     *                            For example, for "F9:C9:C5:5F:73:56 - ch-1", the device MAC
+     *                            address is "F9:C9:C5:5F:73:56" and the channel is "ch-1".
+     */
+    public void selectDevice(String deviceMacAndChannel) {
+        /*
+        Temporary array to hold the mac and channel for further processing.
+        Results in [XX:XX:XX:XX:XX:XX, ch-y].
+         */
+        String[] temp = deviceMacAndChannel.split(" - ");
+        Log.d(TAG, "Bridge, temp.length = " + temp.length);
+        for(int i = 0; i < temp.length; i++) {
+            Log.d(TAG, "Bridge, before: temp[" + i + "]=" + temp[i] + ", length=" + temp[i].length());
+        }
+
+        gameSelectedDeviceMac = temp[0];
+        /*
+        Parsing the channel string and converting it into an integer.
+        Start with "ch-y", then split it into [ch, y], then take the second element.
+         */
+        gameSelectedDeviceChannel = Integer.parseInt((temp[1].split("-"))[1]);
+        Log.d(TAG, "Bridge, after: Mac=" + gameSelectedDeviceMac + ", length=" + gameSelectedDeviceMac.length());
+        Log.d(TAG, "Bridge, after: ch=" + gameSelectedDeviceChannel);
+
+        /*
+        Register the power observer for the emg power callback to stream the emg power data.
+         */
+//        try {
+//            service.registerEmgPwrObserver(pwrObserver);
+//        } catch (RemoteException e) {
+//            e.printStackTrace();
+//        }
     }
 }
