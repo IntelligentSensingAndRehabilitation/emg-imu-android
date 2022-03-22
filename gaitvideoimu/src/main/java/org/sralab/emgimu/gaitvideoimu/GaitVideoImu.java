79d79db9c2fa48289a30f417b5bde837477918e5
Victor Stepanov <vstepanov@ricres.org>
2022-03-22 13:15:18 -0500

    Android: Cleanup code/cmts
    [GUIS-15]

--- gaitvideoimu/src/main/java/org/sralab/emgimu/gaitvideoimu/GaitVideoImu.java
+++ gaitvideoimu/src/main/java/org/sralab/emgimu/gaitvideoimu/GaitVideoImu.java
@@ -1,25 +1,5 @@
 package org.sralab.emgimu.gaitvideoimu;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.Preview;
-import androidx.camera.video.VideoCapture;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.video.MediaStoreOutputOptions;
-import androidx.camera.video.Quality;
-import androidx.camera.video.QualitySelector;
-import androidx.camera.video.Recorder;
-import androidx.camera.video.Recording;
-import androidx.camera.video.RecordingStats;
-import androidx.camera.video.VideoRecordEvent;
-import androidx.camera.view.PreviewView;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.Manifest;
 import android.content.ContentValues;
 import android.content.pm.PackageManager;
@@ -32,6 +12,25 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.video.MediaStoreOutputOptions;
+import androidx.camera.video.Quality;
+import androidx.camera.video.QualitySelector;
+import androidx.camera.video.Recorder;
+import androidx.camera.video.Recording;
+import androidx.camera.video.RecordingStats;
+import androidx.camera.video.VideoCapture;
+import androidx.camera.video.VideoRecordEvent;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
@@ -42,6 +41,7 @@ import com.google.firebase.storage.StorageReference;
 import com.google.gson.Gson;
 
 import org.jetbrains.annotations.NotNull;
+import org.sralab.emgimu.gaitvideoimu.databinding.ActivityGaitVideoImuBinding;
 import org.sralab.emgimu.gaitvideoimu.stream_visualization.DeviceViewModel;
 import org.sralab.emgimu.gaitvideoimu.stream_visualization.StreamingAdapter;
 import org.sralab.emgimu.logging.FirebaseGameLogger;
@@ -49,7 +49,6 @@ import org.sralab.emgimu.logging.FirebaseGameLogger;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -57,18 +56,14 @@ import java.util.concurrent.Executors;
 import kotlin.jvm.internal.Intrinsics;
 import no.nordicsemi.android.nrftoolbox.widget.DividerItemDecoration;
 
-import org.sralab.emgimu.gaitvideoimu.databinding.ActivityGaitVideoImuBinding;
-
 public class GaitVideoImu extends AppCompatActivity {
 
-    private static String TAG = GaitVideoImu.class.getSimpleName();
+    private static final String TAG = GaitVideoImu.class.getSimpleName();
 
     private DeviceViewModel dvm;
     private StreamingAdapter streamingAdapter;
 
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
-    private PreviewView previewView;
-    //private VideoCapture videoCapture;
     @Nullable
     private Recording recording;
 
@@ -77,7 +72,6 @@ public class GaitVideoImu extends AppCompatActivity {
         public long startTime;
         public long endTime;
         public long videoRecordEventStartTime;
-        public long phoneInternalStorageFilename;
     }
     ArrayList<GaitTrial> trials = new ArrayList<>();
     private GaitTrial curTrial;
@@ -94,7 +88,6 @@ public class GaitVideoImu extends AppCompatActivity {
             Manifest.permission.CAMERA,
             Manifest.permission.RECORD_AUDIO,
             Manifest.permission.WRITE_EXTERNAL_STORAGE};
-    //private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private ExecutorService cameraExecutor;
     @Nullable
     private VideoCapture<Recorder> videoCapture;
@@ -195,7 +188,7 @@ public class GaitVideoImu extends AppCompatActivity {
 
     }
 
-    private final void stopCaptureVideo() {
+    private void stopCaptureVideo() {
         Recording curRecording = recording;
         if (curRecording != null) {
             // Stop the current recording session.
@@ -209,6 +202,7 @@ public class GaitVideoImu extends AppCompatActivity {
     private final void captureVideo() {
         viewBinding.startButton.setEnabled(false);
 
+        // if videoCapture is already running (for some reason) stop it
         Recording curRecording = recording;
         if (curRecording != null) {
             // Stop the current recording session.
@@ -217,13 +211,9 @@ public class GaitVideoImu extends AppCompatActivity {
             return;
         }
 
-        // James' logging code
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd-HHmmss'Z'");
         Date now = new Date();
-
-
         String fileName = formatter.format(now) + ".mp4";
-
         String uploadFileName =  "videos/" + mUser.getUid() + "/" + fileName;
 
         curTrial = new GaitTrial();
@@ -236,7 +226,7 @@ public class GaitVideoImu extends AppCompatActivity {
         contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName);
         contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4");
 
-        // end James logging code
+
         // timestamping code for comparison
         Long startTime_ms1 = now.getTime();
         Long startTime_us1 = System.nanoTime();
@@ -255,28 +245,21 @@ public class GaitVideoImu extends AppCompatActivity {
                 .start(ContextCompat.getMainExecutor(this), videoRecordEvent -> {
                     if (videoRecordEvent instanceof VideoRecordEvent.Start) {
                         // Handle the start of a new active recording
-                        // Leaving this for now, when we want to use stop/pause button
-                        // then we can save UI space by reusing the buttons
-/*                        viewBinding.videoCaptureButton.setText(R.string.stop_capture);
-                        viewBinding.videoCaptureButton.setEnabled(true);*/
-                        // Once the recording has started, we disable the start button and
-                        // enable the stop button
                         viewBinding.startButton.setEnabled(false);
                         viewBinding.stopButton.setEnabled(true);
+                        Date tsFromVideoRecordEvent = new Date();
+                        curTrial.videoRecordEventStartTime = tsFromVideoRecordEvent.getTime();
 
                         // code for timestamp comparison
                         Date time_ms2 = new Date();
                         Long startTime_ms2 = time_ms2.getTime();
-                        Long timestampDifference_ms = startTime_ms2 - startTime_ms1;
+                        long timestampDifference_ms = startTime_ms2 - startTime_ms1;
                         Long startTime_us2 = System.nanoTime();
-                        Long timestampDifference_us = startTime_us2 - startTime_us1;
+                        long timestampDifference_us = startTime_us2 - startTime_us1;
                         Log.d(TAG, "timestamp Intent = " + startTime_ms1);
                         Log.d(TAG, "timestamp inside VideoRecordEvent = " + startTime_ms2);
                         Log.d(TAG, "timestampDifference_ms = " + timestampDifference_ms + " ms. Note: using Date class, millisecond precision");
                         Log.d(TAG, "timestampDifference_us = " + timestampDifference_us + " us. Note: using System class, nanosecond precision");
-
-                        Date tsFromVideoRecordEvent = new Date();
-                        curTrial.videoRecordEventStartTime = tsFromVideoRecordEvent.getTime();
                     }
                     else if (videoRecordEvent instanceof VideoRecordEvent.Pause) {
                         // Handle the case where the active recording is paused
@@ -295,13 +278,8 @@ public class GaitVideoImu extends AppCompatActivity {
                             Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
                             Log.d(TAG, msg);
 
-                            // logging code here
                             showVideoStatus("Uploading " + fileName);
-
                             Uri file = ((VideoRecordEvent.Finalize) videoRecordEvent).getOutputResults().getOutputUri();
-
-                            Log.d(TAG, "Filterforme: " + file.getPath() + " " + file.toString());
-                            Log.d(TAG, "Filterforme: " + ((VideoRecordEvent.Finalize) videoRecordEvent).getOutputResults());
                             StorageReference storageRef = storage.getReference().child(uploadFileName);
                             storageRef.putFile(file)
                                     .addOnFailureListener(e -> showVideoStatus("Upload "  + fileName + " failed"))
@@ -356,7 +334,7 @@ public class GaitVideoImu extends AppCompatActivity {
         }, ContextCompat.getMainExecutor(getBaseContext()));
     }
 
-    private final boolean allPermissionsGranted() {
+    private boolean allPermissionsGranted() {
         return (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED ||
                 ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED ||
                 ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
@@ -367,7 +345,7 @@ public class GaitVideoImu extends AppCompatActivity {
         super.onDestroy();
         ExecutorService executorService = this.cameraExecutor;
         if (executorService == null) {
-            Intrinsics.throwUninitializedPropertyAccessException((String)"cameraExecutor");
+            Intrinsics.throwUninitializedPropertyAccessException("cameraExecutor");
             executorService = null;
         }
         executorService.shutdown();
